8e47ff7267e38af0008fdfb9c85948a94e99fe24
PawelPabianczyk <pawel.pabianczyk1999@gmail.com>
2020-02-15 01:04:42 +0100

    exercise 1 add Customers/Details/id

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,13 +12,7 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            //routes.MapMvcAttributeRoutes(); 
-
-            routes.MapRoute(
-                "CustomersDetails",
-                "Customers/Details/{id}",
-                new {controller = "Home", action = "ByCustomersDetails"});            
+          
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
