2552dceac6c532c6cb8c6055d7276724c1866695
PawelPabianczyk <pawel.pabianczyk1999@gmail.com>
2020-02-16 14:14:07 +0100

    Add database migrations

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -21,53 +21,5 @@ namespace Vidly.Controllers
         { 
             return View();
         }
-
-        //public ActionResult Customers()
-        //{
-        //    //var customers = _context.Customers;
-
-        //    var customers = new List<Customer>
-        //    {
-        //        new Customer {Name = "Paweł Pabiańczyk", Id = 1},
-        //        new Customer {Name = "Julia Migdał", Id = 2}
-        //    };
-
-        //    var viewModel = new CustomersViewModel
-        //    {
-        //        Customers = customers
-        //    };
-
-        //    return View(viewModel);
-        //}
-
-        //public ActionResult Movies()
-        //{
-        //    var movies = new List<Movie>
-        //    {
-        //        new Movie {Name = "Przygody Fabiana"},
-        //        new Movie {Name = "Przygody Fabiana 2"}
-        //    };
-
-        //    var viewModel = new MoviesViewModel
-        //    {
-        //        Movies = movies
-        //    };
-
-        //    return View(viewModel);
-        //}
-
-        //public ActionResult ByCustomersDetails(int id)
-        //{
-        //    var customers = new List<Customer>
-        //    {
-        //        new Customer {Name = "Paweł Pabiańczyk", Id = 1},
-        //        new Customer {Name = "Julia Migdał", Id = 2}
-        //    };
-
-        //    if (id > customers.Count || id<1)
-        //        return HttpNotFound();
-        //    else
-        //        return Content(customers[id-1].Name);
-        //}
     }
 }
